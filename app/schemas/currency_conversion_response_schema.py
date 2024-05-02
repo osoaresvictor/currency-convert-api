8aa0bfc6e4c3af24f2fccaa9abaa76493a4dd01d
Victor Soares <victor_soares@live.com>
2024-05-01 22:06:31 -0300

    chore: CurrencyConversionResponseSchema schema refactoring

--- app/schemas/currency_conversion_response_schema.py
+++ app/schemas/currency_conversion_response_schema.py
@@ -5,9 +5,9 @@ from pydantic import BaseModel
 class CurrencyConversionResponseSchema(BaseModel):
     transaction_id: int | None
     user_id: str | None
-    source_currency: str | None
+    source_currency_code: str | None
     source_currency_value: float | None
-    target_currency: str | None
+    target_currency_code: str | None
     target_currency_value: float | None
     rate_value: float | None
     datetime: datetime | None
